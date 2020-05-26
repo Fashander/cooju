24166edd61c961ac01d511bdde5cd9975e134cb4
Capt Weiss <captweiss@foxmail.com>
2020-05-26 16:26:59 +0100

    check line 7 in the root index file and comment out as instructed to avoid getting the - uncaught error - auth/invalid-api-key - since dot env file is not yet enabled or loaded :pushpin:

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../srcCopy/App';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/main.scss';
-import * as serviceWorker from '../srcCopy/serviceWorker';
-import Firebase, { FirebaseContext } from './components/Firebase';
+import * as serviceWorker from './serviceWorker';
+import Firebase, { FirebaseContext } from './components/Firebase'; // comment out this line, line 15 and line 17 if you haven't set the .env environment
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './assets/color';
 
 
 ReactDOM.render(
     <ThemeProvider theme={theme} >
-        {console.log('superTheme: ', theme)}
+        {console.log('customTheme: ', theme)}
         <FirebaseContext.Provider value={new Firebase()}>
             <App />
         </FirebaseContext.Provider>
