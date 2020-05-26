a10f30773767280ace04f1bcd2a7a4f88899828d
Capt Weiss <captweiss@foxmail.com>
2020-05-26 15:51:23 +0100

    Firebase provider attached to all app components, so as react ThemeProvider. try to use this profile folder for all works relating to the profile page in components and you can import and export them through this index.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from '../srcCopy/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/main.scss';
+import * as serviceWorker from '../srcCopy/serviceWorker';
+import Firebase, { FirebaseContext } from './components/Firebase';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './assets/color';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ThemeProvider theme={theme} >
+        {console.log('superTheme: ', theme)}
+        <FirebaseContext.Provider value={new Firebase()}>
+            <App />
+        </FirebaseContext.Provider>
+    </ThemeProvider>
+    ,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
